4c23f3f290b4d877983a19c5990febe97d58743c
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-04-28 03:45:25 +0100

    refactored initialiseLocalStorage() into a generic function that can be re-used by other functions
    - Created a function to handle form rendering at the end of the quiz -
    
    - Created a second array in local storage called all results, with a function that clears results from the first feedbackResults array at the end and saves the persons name input as well as their answers in a separate array in local storage
    
    - Form section pseudocode in index.html
    
    - Form styling added to styles.css

--- assets/js/index.js
+++ assets/js/index.js
@@ -52,64 +52,128 @@ const questions = [
 // event handler function to handle click events in question section
 // this function decides what happens next
 const handleOptionClick = (event) => {
-  console.log ('clicked somethere in question section')
+  console.log("clicked somewhere in question section");
 
-  // get current target 
+  // get current target
   const currentTarget = event.currentTarget;
 
-  // get target 
+  // get target
   const target = event.target;
-  // console.log (target.tagName)
 
   // check if click originates from li only
   // check if target element is li element
-  if (target.tagName === 'LI') {
+  if (target.tagName === "LI") {
     // get the option the user clicked on
-    const value = target.getAttribute('data-value');
-    console.log(value);
+    const value = target.getAttribute("data-value");
+
     // get the question the user answered
     const question = questions[questionIndex].text;
-    console.log(question);
-    // build an answer object that contains the question and answer
+
+    // build an answer object that contains question and answer
     const answer = {
       question,
       value,
     };
-    // TO DO: store answer in local storage 
-    
-    storeAnswerInLS(answer);
-    
+
+    // store answer in local storage
+    storeInLS("feedbackResults", answer);
+
     // remove question
     removeQuestion();
 
     if (questionIndex < questions.length - 1) {
       // go to next question if not the last question
       // increment the question index by 1
-      questionIndex +=1;
+      questionIndex += 1;
 
       // render question
       renderQuestion();
-      
     } else {
-      // if last question, then render results
-     renderResults ();
-
-     renderForm();
-      
+      // if last question then render results and form
+      renderResults();
+      renderForm();
     }
-
   }
-}
+};
 
-// function to render the results of the
-renderResults = () => {
-  console.log('render results')
-}
+const handleFormSubmit = (event) => {
+  event.preventDefault();
 
-// function to render the form 
-renderForm = () => {
-  console.log('render form')
-}
+  // get full name from input
+  const fullName = document.getElementById("full-name").value;
+
+  // validate
+  if (fullName) {
+    // if valid then store feedbackResults in LS
+    const feedbackResults = JSON.parse(localStorage.getItem("feedbackResults"));
+
+    // build object with fullName and results
+    const result = {
+      fullName,
+      feedbackResults,
+    };
+
+    // push the results back to LS
+    storeInLS("allResults", result);
+
+    // clear feedbackResults
+    localStorage.removeItem("feedbackResults");
+
+    // remove form
+    document.getElementById("feedback-form").remove();
+  } else {
+    alert("Please enter full name!");
+  }
+};
+
+// function to render the results
+const renderResults = () => {
+  console.log("render results");
+};
+
+// function to render the form
+const renderForm = () => {
+  console.log("render form");
+  const section = document.createElement("section");
+  section.setAttribute("class", "feedback-form-section");
+  section.setAttribute("id", "feedback-form");
+
+  const h2 = document.createElement("h2");
+  h2.setAttribute("class", "title");
+  h2.textContent = "Submit your feedback";
+
+  const form = document.createElement("form");
+
+  const inputDiv = document.createElement("div");
+  inputDiv.setAttribute("class", "form-control");
+
+  const input = document.createElement("input");
+  input.setAttribute("id", "full-name");
+  input.setAttribute("class", "form-input");
+  input.setAttribute("type", "text");
+  input.setAttribute("placeholder", "Enter full name");
+
+  inputDiv.append(input);
+
+  const buttonDiv = document.createElement("div");
+  buttonDiv.setAttribute("class", "form-control");
+
+  const button = document.createElement("button");
+  button.setAttribute("type", "submit");
+  button.setAttribute("class", "btn");
+  button.textContent = "Submit";
+
+  buttonDiv.append(button);
+
+  form.append(inputDiv, buttonDiv);
+
+  section.append(h2, form);
+
+  mainElement.append(section);
+
+  // add event listener for form submission
+  form.addEventListener("submit", handleFormSubmit);
+};
 
 // function to render question to page
 const renderQuestion = () => {
@@ -137,19 +201,16 @@ const renderQuestion = () => {
   const li1 = document.createElement("li");
   li1.setAttribute("class", "list-item");
   li1.setAttribute("data-value", currentQuestion.options[0]);
-
   li1.textContent = currentQuestion.options[0];
 
   const li2 = document.createElement("li");
   li2.setAttribute("class", "list-item");
   li2.setAttribute("data-value", currentQuestion.options[1]);
-
   li2.textContent = currentQuestion.options[1];
 
   const li3 = document.createElement("li");
   li3.setAttribute("class", "list-item");
   li3.setAttribute("data-value", currentQuestion.options[2]);
-
   li3.textContent = currentQuestion.options[2];
 
   ul.append(li1, li2, li3);
@@ -160,8 +221,8 @@ const renderQuestion = () => {
   // append question section to main element
   mainElement.append(section);
 
-  // add event listener on question section, but only after the whole section has been rendered
-  section.addEventListener('click', handleOptionClick)
+  // add event listener on question section
+  section.addEventListener("click", handleOptionClick);
 };
 
 // function to remove banner from page
@@ -170,50 +231,47 @@ const removeBanner = () => {
   bannerSection.remove();
 };
 
-// function to remove completed question from the page
+// function to remove question section from page
 const removeQuestion = () => {
   console.log("remove question");
   document.getElementById("question-container").remove();
-
 };
 
-  // Initialise local storage
-  const initialiseLocalStorage = () => {
-    //  get feedback results from LS
-    const feedbackResultsFromLS = JSON.parse(localStorage.getItem("feedbackResults"));
+const initialiseLocalStorage = () => {
+  // get feedbackResults from LS
+  const feedbackResultsFromLS = JSON.parse(
+    localStorage.getItem("feedbackResults")
+  );
 
-    if (!feedbackResultsFromLS) {
-      // if it doesnt exist, set LS to have feedbackResults as an empty array
-      localStorage.setItem("feedbackResults", JSON.stringify([]));
-    }
-    // if it already exists, do nothing
+  const allResultsFromLS = JSON.parse(localStorage.getItem("allResults"));
 
-  };
+  if (!feedbackResultsFromLS) {
+    // if not exist set LS to have feedbackResults as an empty array
+    localStorage.setItem("feedbackResults", JSON.stringify([]));
+  }
 
+  if (!allResultsFromLS) {
+    // if not exist set LS to have feedbackResults as an empty array
+    localStorage.setItem("allResults", JSON.stringify([]));
+  }
+};
 
-  // store answer in Local storage
-  const storeAnswerInLS = (answer) => {
-    // get feedback results from LS
-    const feedbackResults = JSON.parse(localStorage.getItem("feedbackResults"));
-    // push answer to array
-    feedbackResults.push(answer);
-    
-    // set feedbackResults in localStorage
-    localStorage.setItem("feedbackResults", JSON.stringify(feedbackResults));
-  };
+const storeInLS = (key, value) => {
+  // get feedbackResults from LS
+  const arrayFromLS = JSON.parse(localStorage.getItem(key));
 
+  // push answer in to array
+  arrayFromLS.push(value);
+
+  // set feedbackResults in LS
+  localStorage.setItem(key, JSON.stringify(arrayFromLS));
+};
 
 // declare the event handler function for start button click
 const handleStartButtonClick = () => {
   console.log("start button clicked");
-  
-
- // initialise local storage
- initialiseLocalStorage();
-
-
-
-
+  // initialise local storage
+  initialiseLocalStorage();
 
   // remove banner section
   removeBanner();
