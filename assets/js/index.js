84628e4e69bc2fee08c578051c289ffb8d3c4faf
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-04-28 01:53:28 +0100

    Function to render remove answered question and render the next one
    - Created an event handler function to handle click events in the question section. This function decides what happens next
    
    - Added set attribute of data value to each list item, which matches the current question answer options
    
    -   Added event listener on whole question section, but only starts after the whole section has been rendered, and only listens for clicks to child list items

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,6 +49,67 @@ const questions = [
   },
 ];
 
+// event handler function to handle click events in question section
+// this function decides what happens next
+const handleOptionClick = (event) => {
+  console.log ('clicked somethere in question section')
+
+  // get current target 
+  const currentTarget = event.currentTarget;
+
+  // get target 
+  const target = event.target;
+  // console.log (target.tagName)
+
+  // check if click originates from li only
+  // check if target element is li element
+  if (target.tagName === 'LI') {
+    // get the option the user clicked on
+    const value = target.getAttribute('data-value');
+    console.log(value);
+    // get the question the user answered
+    const question = questions[questionIndex].text;
+    console.log(question);
+    // build an answer object that contains the question and answer
+    const answer = {
+      question,
+      value,
+    }
+    // TO DO: store answer in local storage 
+    console.log(answer);
+    
+    // remove question
+    removeQuestion();
+
+    if (questionIndex < questions.length - 1) {
+      // go to next question if not the last question
+      // increment the question index by 1
+      questionIndex +=1;
+
+      // render question
+      renderQuestion();
+      
+    } else {
+      // if last question, then render results
+     renderResults ();
+
+     renderForm();
+      
+    }
+
+  }
+}
+
+// function to render the results of the
+renderResults = () => {
+  console.log('render results')
+}
+
+// function to render the form 
+renderForm = () => {
+  console.log('render form')
+}
+
 // function to render question to page
 const renderQuestion = () => {
   console.log("render question");
@@ -59,6 +120,7 @@ const renderQuestion = () => {
   // create section
   const section = document.createElement("section");
   section.setAttribute("class", "content-section question-container");
+  section.setAttribute("id", "question-container");
 
   // create h2
   const h2 = document.createElement("h2");
@@ -73,14 +135,20 @@ const renderQuestion = () => {
   // TODO: loop over options to create and append li to ul
   const li1 = document.createElement("li");
   li1.setAttribute("class", "list-item");
+  li1.setAttribute("data-value", currentQuestion.options[0]);
+
   li1.textContent = currentQuestion.options[0];
 
   const li2 = document.createElement("li");
   li2.setAttribute("class", "list-item");
+  li2.setAttribute("data-value", currentQuestion.options[1]);
+
   li2.textContent = currentQuestion.options[1];
 
   const li3 = document.createElement("li");
   li3.setAttribute("class", "list-item");
+  li3.setAttribute("data-value", currentQuestion.options[2]);
+
   li3.textContent = currentQuestion.options[2];
 
   ul.append(li1, li2, li3);
@@ -90,12 +158,22 @@ const renderQuestion = () => {
 
   // append question section to main element
   mainElement.append(section);
+
+  // add event listener on question section, but only after the whole section has been rendered
+  section.addEventListener('click', handleOptionClick)
 };
 
 // function to remove banner from page
 const removeBanner = () => {
   console.log("remove banner");
   bannerSection.remove();
+};
+
+// function to remove completed question from the page
+const removeQuestion = () => {
+  console.log("remove banner");
+  document.getElementById("question-container").remove();
+
 };
 
 // declare the event handler function for start button click
